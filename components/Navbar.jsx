09075bc7338339fa9c990f097fb97a8b75d7b00a
hakimisch <hakimizaimi@gmail.com>
2023-07-04 18:39:35 +0800

    added admin dashboard and admin orders list

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -72,6 +72,16 @@ const Navbar = () => {
                                         Order History
                                     </DropdownLink>
                                 </Menu.Item>
+                                {session.user.isAdmin && (
+                                    <Menu.Item>
+                                        <DropdownLink
+                                            className="dropdown-link"
+                                            href="/admin/dashboard"
+                                            >
+                                            Admin Dashboard
+                                        </DropdownLink>
+                                    </Menu.Item>
+                                )}
                                 <Menu.Item>
                                     <a
                                         className="dropdown-link"
